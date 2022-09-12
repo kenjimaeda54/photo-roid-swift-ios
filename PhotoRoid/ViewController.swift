b9c2ec37c2041ea42cd87bb48f80746eb289bbc7
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-09-12 08:59:37 -0300

    Initial Commit

---
+++ PhotoRoid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoRoid
+//
+//  Created by kenjimaeda on 12/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
